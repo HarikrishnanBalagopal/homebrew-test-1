b8680e0fbb95ad630eaf0944bb5aad7a6e24e100
Harikrishnan Balagopal <harikrishmenon@gmail.com>
2021-10-11 18:11:34 +0530

    feat: 2nd version
    Signed-off-by: Harikrishnan Balagopal <harikrishmenon@gmail.com>

---
+++ Formula/gothanks@v0.1.0-alpha.1.rb
@@ -0,0 +1,18 @@
+class Gothanks < Formula
+  desc "Automatically star your go.mod Github dependencies"
+  homepage "https://github.com/psampaz/gothanks"
+  url "https://github.com/psampaz/gothanks/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "300b705751a43f6ae25df57d6c9a5b0c859e92d61fa83cf894329ea9662525f2"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args
+  end
+
+  test do
+    ENV.delete "GITHUB_TOKEN"
+    assert_match "no Github token found", shell_output(bin/"gothanks", 255)
+  end
+end
